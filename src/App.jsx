941d8ae9919905c47a20190b222098adac497abc
mmchokor <mohameddd701@gmail.com>
2022-06-27 15:21:01 +0300

    Tailwind & Daisy UI setup - Added Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navbar from './components/layout/Navbar'
 
 function App() {
    return (
       <>
          <Router>
-            <Routes>
-               {/* <Route path='/' element={<Home />} />
+            <div className='flex flex-col justiy-between h-screen'>
+               <Navbar />
+               <main className='container mx-auto px-3 pb-12'>
+                  <Routes>
+                     {/* <Route path='/' element={<Home />} />
                <Route path='/about' element={<About />} />
                <Route path='/contact' element={<Contact />} /> */}
-            </Routes>
+                  </Routes>
+               </main>
+            </div>
          </Router>
       </>
    )
