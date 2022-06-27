f6049676478068369fd748185bc0bc039a856e43
mmchokor <mohameddd701@gmail.com>
2022-06-27 15:00:40 +0300

    Setup some packages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
-   return <div></div>
+   return (
+      <>
+         <Router>
+            <Routes>
+               {/* <Route path='/' element={<Home />} />
+               <Route path='/about' element={<About />} />
+               <Route path='/contact' element={<Contact />} /> */}
+            </Routes>
+         </Router>
+      </>
+   )
 }
 
 export default App
