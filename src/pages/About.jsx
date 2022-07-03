7e62a5bc80f77d1284834beac5053d0a5f1e896a
mmchokor <mohameddd701@gmail.com>
2022-07-03 19:44:01 +0300

    Modified About me page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,26 +1,38 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
 
 function About() {
    return (
-    // center the div belwon
+      // center the div belwon
 
-      <div className='max-w-2xl py-4 px-8 bg-neutral shadow-lg rounded-lg my-20 mx-auto'>
+      <div className='max-w-3xl py-4 px-8 bg-neutral shadow-lg rounded-lg my-20 mx-auto'>
          <div className='flex justify-center md:justify-end -mt-16'>
             <img
                className='w-20 h-20 object-cover rounded-full border-2 border-indigo-500 lg:scale-150 md:scale-125 lg:mr-10 md:mr-7'
-               src='https://images.unsplash.com/photo-1499714608240-22fc6ad53fb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80'
+               src='../src/assets/images/about-image.jpg'
             />
          </div>
          <div className='pt-4'>
             <h2 className='text-white-800 text-3xl font-semibold'>
                Software Developer
             </h2>
-            <p className='mt-2 text-white-600'>
-               Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae
-               dolores deserunt ea doloremque natus error, rerum quas odio
-               quaerat nam ex commodi hic, suscipit in a veritatis pariatur
-               minus consequuntur!
+            <p className='mt-3 text-white-600'>
+               I am a software developer located in Lebanon. I have a passion
+               for for Web Development, solving challenging tasks, and anything
+               programming related.
             </p>
+            <p className='mt-2 text-white-600'>
+               Well organized person, problem solver, independent person with
+               high attention to detail. Big fan of Formula 1, and anything Car
+               related, also have a big passion for tech.
+            </p>
+            <p className='my-2 text-white-600'>
+               Interested in the entire software development life cycle, from
+               requirements gathering to development to deployment and
+               maintenance, and all of that working with positive people.
+            </p>
+            <Link to='/contact' className='text-white-600 text-indigo-500 text-lg'>
+               Let's work on a new project!
+            </Link>
          </div>
          <div className='flex justify-end mt-4'>
             <a href='#' className='text-xl font-medium text-indigo-500'>
