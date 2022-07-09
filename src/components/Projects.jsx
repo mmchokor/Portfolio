e2544e32ded721c72c33a8decce9f054d682d511
mmchokor <mohameddd701@gmail.com>
2022-07-09 16:18:21 +0300

    Added face attendance project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,6 +2,7 @@
 import HouseMarketplaceImage from '../assets/images/projectsImages/hourse-marketplace.jpg'
 import PortfolioImage from '../assets/images/projectsImages/portfolio.png'
 import SupportDesk from '../assets/images/projectsImages/support-desk.jpg'
+import FaceAttendance from '../assets/images/projectsImages/face-attendance.jpg'
 // Images ************************************************************************************
 import { AnimationOnScroll } from 'react-animation-on-scroll'
 import ProjectCard from '../components/shared/ProjectCard'
@@ -27,6 +28,15 @@ const Projects = () => {
          githubLink: 'https://github.com/mmchokor/support-desk-app',
       },
       {
+         title: 'Face Attendance Program',
+         description:
+            'A program that assists University Doctors and teaching assistants with taking student attendance. It will scan students faces using the camera connected to the device on which the program is running, it will find the face in the video and encode it into data that will be compared with the encoding of the images present in the program database, upon detecting the facial identification, it will save the student s name together with the time he entered the class in a csv file that will be sent to the teacher via Email, after the software is closed by scanning the instructors face.',
+         image: FaceAttendance,
+         technologies: ['Python', 'OpenCV', 'face_recognition', 'Numpy'],
+         demoLink: '',
+         githubLink: 'https://github.com/mmchokor/Face-Attendace-Program',
+      },
+      {
          title: 'Chokor.Portfolio()',
          description: 'The website you are currently viewing',
          image: PortfolioImage,
@@ -40,7 +50,6 @@ const Projects = () => {
          <AnimationOnScroll animateOnce={true} animateIn='animate__fadeInUp'>
             <div className='mb-10 text-center text-4xl text-white font-bold bg-gray-900 border border-gray-700 py-6 rounded-lg shadow-md'>
                <h1>My Projects</h1>{' '}
-               <span className='text-sm'>(Not all project are added yet)</span>
             </div>
          </AnimationOnScroll>
 
