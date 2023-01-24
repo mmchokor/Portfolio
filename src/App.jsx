c3a68ae25536a1746ad42ed7a686763a29cf1e93
mmchokor <mohameddd701@gmail.com>
2023-01-24 20:43:50 +0200

    fixed typo

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,22 @@
 // It also contains the Navbarar and Footer component
 // It is all contained in a flexbox container
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import Contact from './pages/Contact'
-import About from './pages/About'
-import Resume from './pages/Resume'
-import Navbar from './components/layout/Navbar'
-import Footer from './components/layout/Footer'
-import './index.css'
 import "animate.css/animate.min.css"
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import Footer from './components/layout/Footer'
+import Navbar from './components/layout/Navbar'
+import './index.css'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Home from './pages/Home'
+import Resume from './pages/Resume'
 
 function App() {
    return (
       <>
          <Router>
             <div className='flex flex-col justiy-between min-h-screen bg-gray-800'>
-               <Navbar title={'Chokor.Protfolio()'} />
+               <Navbar title={'Chokor.Portfolio()'} />
                <main className='container mx-auto px-3 pb-12 flex-grow'>
                   <Routes>
                      <Route path='/' element={<Home />} />
