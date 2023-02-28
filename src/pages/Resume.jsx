cfc0da8135969662913ed5c0e8a746a73e1a3585
mmchokor <mohameddd701@gmail.com>
2023-02-28 11:42:33 +0200

    Modified Resume - added version number

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -7,11 +7,11 @@ import { useState } from 'react'
 const Resume = () => {
    // Link for the iframe to view my resume from my google drive 
    const [resumeLink] = useState(
-      'https://drive.google.com/file/d/1rw68HQQ9pimlyamq9jMwG8qGXmM8QfVr/preview'
+      'https://drive.google.com/file/d/1SmtbTnz4NgZ4YDGiXCd8K9ybVRUKsFJU/preview'
    )
    // Link to be used in the a tag to directly download my resume
    const [resumeLinkDownload] = useState(
-      'https://drive.google.com/uc?export=download&id=1rw68HQQ9pimlyamq9jMwG8qGXmM8QfVr'
+      'https://drive.google.com/uc?export=download&id=1SmtbTnz4NgZ4YDGiXCd8K9ybVRUKsFJU'
    )
    const [loading, setLoading] = useState(true)
 
