82c640330f5e1ed9cda65e857a44f0742a5579e4
mmchokor <mmchokor@gmail.com>
2025-12-03 13:33:32 +0200

    update projects data

--- src/constants/projectsData.js
+++ src/constants/projectsData.js
@@ -30,24 +30,6 @@ export const projects = [
          'https://expo.dev/accounts/ramiskakini/projects/Badde-Salemtak/builds/417595ec-8122-40e4-85ea-91e756a3ef98',
    },
    {
-      title: 'House Marketplace',
-      description:
-         'A website that enables users to purchase, sell, and rent out their homes. A person may sign up for an account with his or her email or Google account and display their home with all of its characteristics. The user may also browse all of the houses that other users have uploaded, as well as search for residences and see which have offers. The user may also view the houses he has uploaded and edit the ones he has placed. The user can also remove the house that he has listed.',
-      image: HouseMarketplaceImage,
-      technologies: ['ReactJS', 'Firebase', 'NodeJS'],
-      demoLink: 'https://chokor-house-marketplace.netlify.app/',
-      githubLink: 'https://github.com/mmchokor/house_marketplace',
-   },
-   {
-      title: 'Support Desk',
-      description:
-         'A full stack application that allows users to create tickets and view all tickets that have been created. The user can also view all tickets that have been assigned to him or her and view the tickets that have been resolved.',
-      image: SupportDesk,
-      technologies: ['ReactJS', 'NodeJS', 'ExpressJS', 'MongoDB', 'Vite'],
-      demoLink: 'https://support-desk-frontend-chokor.vercel.app/',
-      githubLink: 'https://github.com/mmchokor/support-desk-app',
-   },
-   {
       title: 'Face Attendance Program',
       description:
          'A program that assists University Doctors and teaching assistants with taking student attendance. It will scan students faces using the camera connected to the device on which the program is running, it will find the face in the video and encode it into data that will be compared with the encoding of the images present in the program database, upon detecting the facial identification, it will save the student s name together with the time he entered the class in a csv file that will be sent to the teacher via Email, after the software is closed by scanning the instructors face.',
@@ -57,20 +39,12 @@ export const projects = [
       githubLink: 'https://github.com/mmchokor/Face-Attendace-Program',
    },
    {
-      title: 'Pharmacy Admin Panel',
+      title: 'House Marketplace',
       description:
-         'A website to manage all the data of a pharmacy. It was made using PHP, HTML, CSS, JavaScript, and SQL.',
-      image: PharmacyMS,
-      technologies: ['PHP', 'SQL', 'HTML', 'Bootstrap', 'CSS', 'JavaScript'],
-      demoLink: '',
-      githubLink: 'https://github.com/mmchokor/Pharmacy-Database-Project',
-   },
-   {
-      title: 'Chokor.Portfolio()',
-      description: 'The website you are currently viewing',
-      image: PortfolioImage,
-      technologies: ['ReactJS', 'Tailwind Css', 'NodeJS', 'Vite'],
-      demoLink: 'https://mohamed-chokor.netlify.app/',
-      githubLink: 'https://github.com/mmchokor/Portfolio',
+         'A website that enables users to purchase, sell, and rent out their homes. A person may sign up for an account with his or her email or Google account and display their home with all of its characteristics. The user may also browse all of the houses that other users have uploaded, as well as search for residences and see which have offers. The user may also view the houses he has uploaded and edit the ones he has placed. The user can also remove the house that he has listed.',
+      image: HouseMarketplaceImage,
+      technologies: ['ReactJS', 'Firebase', 'NodeJS'],
+      demoLink: 'https://chokor-house-marketplace.netlify.app/',
+      githubLink: 'https://github.com/mmchokor/house_marketplace',
    },
 ]
